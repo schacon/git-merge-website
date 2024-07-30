385d9a1445dd98b5b1a635462dbcae5d2d6c3bdf
Scott Chacon <schacon@gmail.com>
2024-07-30 11:27:08 +0200

    add tracking

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,8 @@
 	import '../styles/main.css';
 </script>
 
+<script async src="https://u.gitbutler.com/script.js" data-website-id="63698491-2913-475d-b965-c7bc9f6aab9e"></script>
+
 <div>
 	<slot></slot>
 </div>
